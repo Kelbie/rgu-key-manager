8fa83e02560bfac401c8ec73715e39780121f05e
kevin <notkelbie@outlook.com>
2018-09-30 05:13:33 +0100

    faster transitions when clicking between tabs
    when clicking tabs it used to generate the html, now it just changes it from display grid to display block.

--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import MaterialIcon from 'material-icons-react';
 
 import Action from '../Action/Action';
+import KeyObject from '../KeyObject/KeyObject';
 
 import styles from './InfoPage.module.css';
 
@@ -12,7 +13,9 @@ class InfoPage extends Component {
     this.tabClicked = this.tabClicked.bind(this)
     this.generateTabs = this.generateTabs.bind(this)
     this.generateFeed = this.generateFeed.bind(this)
-    this.state = {selected: ["history"]};
+    this.state = {
+      selected: ["history"]
+    };
   }
 
   tabClicked(obj) {
@@ -27,19 +30,15 @@ class InfoPage extends Component {
     var tabsArray = [];
     for (var i = 0; i < tabs.length; i++) {
       if (tabs[i] == this.state.selected) {
-        tabsArray.push(
-          <span id={tabs[i]} className={`${styles.tab} + ${styles.selected}`} onClick={this.tabClicked}>
-            {tabs[i]}
-            <div className={`${styles.underline}`}></div>
-          </span>
-        )
+        tabsArray.push(<span id={tabs[i]} className={`${styles.tab} + ${styles.selected}`} onClick={this.tabClicked}>
+          {tabs[i]}
+          <div className={`${styles.underline}`}></div>
+        </span>)
       } else {
-        tabsArray.push(
-          <span id={tabs[i]} className={`${styles.tab}`} onClick={this.tabClicked}>
-            {tabs[i]}
-            <div className={`${styles.underline}`}></div>
-          </span>
-        )
+        tabsArray.push(<span id={tabs[i]} className={`${styles.tab}`} onClick={this.tabClicked}>
+          {tabs[i]}
+          <div className={`${styles.underline}`}></div>
+        </span>)
       }
     }
 
@@ -48,35 +47,38 @@ class InfoPage extends Component {
 
   generateFeed(tabs) {
     var feed = [];
+    var content = {
+      history: [
+        <Action author={"ShonaLilly"} keyid={"80"} index={1} isLost={false} place={"Cupboard"} receiver={"EMatheson"} time={"4 hours ago"}/>,
+        <Action author={"ShonaLilly"} keyid={"80"} index={2} isLost={false} place={"Cupboard"} receiver={"ShonaLilly"} time={"4 hours ago"}/>,
+        <Action author={"ShonaLilly"} keyid={"11254"} index={1} isLost={false} place={"GREEN_ROOM_CB"} receiver={"ShonnaLilly"} time={"4 hours ago"}/>,
+        <Action author={"ShonaLilly"} keyid={"SK54"} index={1} isLost={false} place={"IDEAS_MASTER"} receiver={"ShonaLilly"} time={"4 hours ago"}/>,
+        <Action author={"ShonaLilly"} keyid={"PQ858"} index={1} isLost={false} place={"KITCHEN_CB"} receiver={"VDawod"} time={"4 hours ago"}/>,
+        <Action author={"ShonaLilly"} keyid={"D3108"} index={1} isLost={false} place={"N329"} receiver={"VDawod"} time={"4 hours ago"}/>,
+        <Action author={"ShonaLilly"} keyid={"D3062"} index={1} isLost={false} place={"N332"} receiver={"VDawod"} time={"4 hours ago"}/>
+      ],
+      keys: [
+        <KeyObject/>,
+        <KeyObject/>,
+        <KeyObject/>,
+        <KeyObject/>,
+        <KeyObject/>,
+        <KeyObject/>,
+        <KeyObject/>,
+        <KeyObject/>,
+        <KeyObject/>
+      ]
+    }
     for (var i = 0; i < tabs.length; i++) {
-      switch (tabs[i]) {
-        case "history":
-          var content = [
-            <Action author={"ShonaLilly"} keyid={"80"} index={1} isLost={false} place={"Cupboard"} receiver={"EMatheson"} time={"4 hours ago"}/>,
-            <Action author={"ShonaLilly"} keyid={"80"} index={2} isLost={false} place={"Cupboard"} receiver={"ShonaLilly"} time={"4 hours ago"}/>,
-            <Action author={"ShonaLilly"} keyid={"11254"} index={1} isLost={false} place={"GREEN_ROOM_CB"} receiver={"ShonnaLilly"} time={"4 hours ago"}/>,
-            <Action author={"ShonaLilly"} keyid={"SK54"} index={1} isLost={false} place={"IDEAS_MASTER"} receiver={"ShonaLilly"} time={"4 hours ago"}/>,
-            <Action author={"ShonaLilly"} keyid={"PQ858"} index={1} isLost={false} place={"KITCHEN_CB"} receiver={"VDawod"} time={"4 hours ago"}/>,
-            <Action author={"ShonaLilly"} keyid={"D3108"} index={1} isLost={false} place={"N329"} receiver={"VDawod"} time={"4 hours ago"}/>,
-            <Action author={"ShonaLilly"} keyid={"D3062"} index={1} isLost={false} place={"N332"} receiver={"VDawod"} time={"4 hours ago"}/>,
-          ]
-          break;
-        case "keys":
-          var content = ""
-          break
-        default:
-          var content = ""
-      }
+      console.log(tabs[i], this.state.selected);
       if (tabs[i] == this.state.selected) {
-        feed.push(
-          <div className={`${styles.selected}`}>
-            {content}
-          </div>
-        );
+        feed.push(<div className={`${styles.selected} ${styles[tabs[i]]}`}>
+          {content[tabs[i]]}
+        </div>)
       } else {
-        feed.push(
-          <div></div>
-        );
+        feed.push(<div className={`${styles.deselected}`}>
+          {content[tabs[i]]}
+        </div>)
       }
     }
     return feed;
