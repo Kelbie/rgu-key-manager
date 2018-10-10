ee0a45bea7ffb455ac3836df1200e0f926651c13
Dan1003 <daniolek12345@gmail.com>
2018-10-10 23:53:13 +0100

    react-tabs test

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,10 @@
+//remember to install react-tabs
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 import styles from './Dashboard.module.scss';
 
@@ -11,7 +16,19 @@ class Dashboard extends Component {
   render() {
     return (
       <div className={`${styles.Dashboard}`}>
-        <h1>Dashboard</h1>
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+      <TabPanel>
+        <h2>Any content 1</h2>
+      </TabPanel>
+      <TabPanel>
+        <h2>Any content 2</h2>
+      </TabPanel>
+    </Tabs>
       </div>
     );
   }
