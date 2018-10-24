95a746d3153c5fd1cc91c6b7a8848b97ec302c86
Gaëtan <serial.crossfire@gmail.com>
2018-10-24 13:43:06 +0100

    fix

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,18 +3,12 @@ import {  BrowserRouter as Router, Route, Switch, withRouter } from "react-route
 
 // Router components
 import AboutPage from '../AboutPage/AboutPage';
-import User from '../User/User';
-import Key from '../Key/Key';
-import Place from '../Place/Place';
 
 class Container extends Component {
     render() {
         return (
             <div>
                 <Switch>
-                    <Route path="/user/:username" component={User}/>
-                    <Route path="/key/:keyid" component={Key}/>
-                    <Route path="/place/:place" component={Place}/>
                     <Route path="/about" component={AboutPage} />
                 </Switch>
             </div>
