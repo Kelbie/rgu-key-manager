252aee4ad3e6eb3f67e201cf84553629884a91b3
kevin <notkelbie@outlook.com>
2018-10-27 15:49:50 +0100

    use proper format for table in fobs page

--- src/pages/Fobs/Fobs.js
+++ src/pages/Fobs/Fobs.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import AppLogo from '../../app-logo.svg';
 
@@ -6,24 +6,79 @@ import AppLogo from '../../app-logo.svg';
 import Table from '../../components/Table/Table';
 import Button from '../../components/Button/HeaderButton';
 
-class Fobs extends Component {
-    render(){
-        return (
-          <>
-            <Button variant="outlined" text={"test"}/>
-            <Table path="key"
-              columns={["FOB ID", "HOLDER"]}
-              rows={[
-                [<Button variant="flat" onClick={() => window.location.href="fob/89412"} text="89412" />, <Button variant="flat" onClick={() => window.location.href="user/ExternalExaminers"} text="ExternalExaminers" />],
-                [<Button variant="flat" onClick={() => window.location.href="fob/89412"} text="78920" />, <Button variant="flat" onClick={() => window.location.href="user/JessicaCampbell"} text="JessicaCampbell" />],
-                [<Button variant="flat" onClick={() => window.location.href="fob/89412"} text="55846" />, <Button variant="flat" onClick={() => window.location.href="user/JoBloggs"} text="JoBloggs" />],
-                [<Button variant="flat" onClick={() => window.location.href="fob/89412"} text="38945" />, <Button variant="flat" onClick={() => window.location.href="user/N436AccessFob"} text="N436AccessFob" />],
-                [<Button variant="flat" onClick={() => window.location.href="fob/89412"} text="10258" />, <Button variant="flat" onClick={() => window.location.href="user/StacyGoodman"} text="StacyGoodman" />],
-                [<Button variant="flat" onClick={() => window.location.href="fob/89412"} text="88614" />, <Button variant="flat" onClick={() => window.location.href="user/StaffFob"} text="StaffFob" />],
-              ]}/>
-            </>
-        );
+var fobs = [
+  [
+    {
+      text: "89412",
+      type: "button",
+      linkTo: "fob/89412"
+    }, {
+      text: "ExternalExaminers",
+      type: "plain"
     }
+  ],
+  [
+    {
+      text: "78920",
+      type: "button",
+      linkTo: "fob/78920"
+    }, {
+      text: "JessicaCampbell",
+      type: "button",
+      linkTo: "user/JessicaCampbell"
+    }
+  ],
+  [
+    {
+      text: "55846",
+      type: "button",
+      linkTo: "fob/55846"
+    }, {
+      text: "JoBloggs",
+      type: "button",
+      linkTo: "user/JoBloggs"
+    }
+  ],
+  [
+    {
+      text: "38945",
+      type: "button",
+      linkTo: "fob/38945"
+    }, {
+      text: "N436AccessFob",
+      type: "plain"
+    }
+  ],
+  [
+    {
+      text: "10258",
+      type: "button",
+      linkTo: "fob/10258"
+    }, {
+      text: "StacyGoodman",
+      type: "button",
+      linkTo: "user/StacyGoodman"
+    }
+  ],
+  [
+    {
+      text: "88614",
+      type: "button",
+      linkTo: "fob/88614"
+    }, {
+      text: "StaffFob",
+      type: "plain"
+    }
+  ]
+]
+
+class Fobs extends Component {
+  render() {
+    return (<> < Button variant = "outlined" text = {
+      "test"
+    } /> <Table path="key" columns={["FOB ID", "HOLDER"]} rows={fobs}/>
+  </>);
+  }
 
 }
 export default Fobs;
