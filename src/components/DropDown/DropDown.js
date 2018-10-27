2a7c33fe48a31fd673bd9dbecc5c89bd94413cd7
kevin <notkelbie@outlook.com>
2018-10-27 16:17:18 +0100

    allow icons in more button

--- src/components/DropDown/DropDown.js
+++ src/components/DropDown/DropDown.js
@@ -3,6 +3,28 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import HeaderButton from '../Button/HeaderButton';
+import { withStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceICon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import WarningIcon from '@material-ui/icons/Warning';
+import SendIcon from '@material-ui/icons/Send';
+import FolderIcon from '@material-ui/icons/Folder';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SettingsBackupRestoreIcon from '@material-ui/icons/SettingsBackupRestore';
+
+const styles = theme => ({
+  testest: {
+    margin: theme.spacing.unit,
+  },
+  leftIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
 
 class SimpleMenu extends React.Component {
   state = {
@@ -19,6 +41,10 @@ class SimpleMenu extends React.Component {
 
   render() {
     const { anchorEl } = this.state;
+    const { classes } = this.props;
+    const mapping = {
+      'warning': <WarningIcon className={classes.leftIcon} />
+    }
 
     return (
       <div>
@@ -33,13 +59,14 @@ class SimpleMenu extends React.Component {
 
         </HeaderButton>
         <Menu
+          className={classes.testest}
           id="simple-menu"
           anchorEl={anchorEl}
           open={Boolean(anchorEl)}
           onClose={this.handleClose}
         >
         {this.props.items.map(item => {
-          return <MenuItem onClick={this.handleClose}>{item.text}</MenuItem>
+          return <MenuItem onClick={this.handleClose}>{mapping[item.icon]} {item.text.toUpperCase()}</MenuItem>
         })}
         </Menu>
       </div>
@@ -47,4 +74,4 @@ class SimpleMenu extends React.Component {
   }
 }
 
-export default SimpleMenu;
+export default withStyles(styles)(SimpleMenu);
