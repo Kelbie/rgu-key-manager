9541cbd181ee13ed772612f6df1855ed1ab51ea1
kevin <notkelbie@outlook.com>
2018-10-29 00:26:51 +0000

    make user history use database

--- src/objects/User/User.js
+++ src/objects/User/User.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import MaterialIcon from 'material-icons-react';
+
+import firebase, {auth, database, firestore} from "../../components/Firebase/Firebase";
 
 // Layout Components
 import InfoPage from '../../pages/InfoPage/InfoPage';
@@ -8,21 +10,79 @@ import InfoPage from '../../pages/InfoPage/InfoPage';
 import styles from './User.module.scss';
 
 class User extends Component {
+  state = {history: []}
   constructor() {
     super();
+  }
+
+  componentWillMount() {
+    this.getHistory()
+  }
+
+  async getHistory() {
+    const historyRef = await firestore.collection('history').where('to', '==', this.props.match.params.username)
+
+    try {
+      const snap = await historyRef.get()
+      snap.forEach(row => {
+        row = row.data()
+        this.state.history.push({
+          author: {
+            text: row.author,
+            type: "button",
+            linkTo: "/user/" + row.author
+          },
+          keyid: {
+            text: row.keyid,
+            type: "button",
+            linkTo: "/key/" + row.keyid
+          },
+          to: {
+            text: row.to,
+            type: "button",
+            linkTo: "/user/" + row.to
+          },
+          comment: {
+            text: row.comment
+          },
+          time: {
+            text: row.time
+          }
+        })
+      })
+    } catch (e) {
+      console.log(e)
+    }
+    this.setState({
+      history: this.state.history.sort(function(a, b) {
+        return new Date(b.time.text) > new Date(a.time.text);
+      })
+    })
   }
 
   render() {
     return (
       <InfoPage type={"user"}
                 routeParams={this.props}
-                history={[]}
+                desc={"awd"}
+                history={this.state.history}
                 id={this.props.match.params.username}
-                image={<img src={"http://robohash.org/" + this.props.match.params.username + "?bgset=bg2"} />}
-                buttons={[{text: "Manage Keys", icon: "vpn_key"}, {text: "Invite", icon: "person_add", type: "dialog"}, {text: "test", icon: "person_add"}]} />
-
-    );
-  }
+                image={<img src={"http://robohash.org/" + this.props.match.params.username + "?bgset=bg2"}/>}
+                buttons={[
+                  {
+                    text: "Manage Keys",
+                    icon: "vpn_key"
+                  }, {
+                    text: "Invite",
+                    icon: "person_add",
+                    type: "dialog"
+                  }, {
+                    text: "test",
+                    icon: "person_add"
+                  }]
+                }
+        />);
+    }
 }
 
 export default User;
