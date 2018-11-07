d386591595d2a3759f784391cdd41c0889bbd043
Gaëtan <serial.crossfire@gmail.com>
2018-11-07 13:05:47 +0000

    UserPage update

--- src/pages/UsersPage/UserCard.js
+++ src/pages/UsersPage/UserCard.js
@@ -2,59 +2,38 @@ import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import classnames from 'classnames';
 
 import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import EmailIcon from '@material-ui/icons/Email';
 import Typography from "@material-ui/core/Typography";
-import IconPerson from "./icon_person.svg";
+import IconPerson from "@material-ui/icons/Person";
+import { Button, ListItem, ListItemIcon, ListItemText, Avatar, Grid } from '@material-ui/core';
 
 const styles = theme => ({
-    cardHead: {
-        display: 'flex',
+    card:{
+        width: 310
     },
     avatar: {
         width: 100,
         height: 100,
     },
-    content: {
-        width: 180,
-        height: 110,
+    icon: {
+        width: 90,
+        height: 90,
     },
-    actions: {
-        display: 'flex',
+    mail: {
+        top: 15
     },
-    expand: {
-        transform: 'rotate(0deg)',
-        transition: theme.transitions.create('transform', {
-          duration: theme.transitions.duration.shortest,
-        }),
-        marginLeft: 'auto',
-        [theme.breakpoints.up('sm')]: {
-          marginRight: -25,
-        },
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    delete: {
-        marginLeft: 'auto',
+    grow: {
+        flexGrow: 1,
     },
 });
 
 class UserCard extends Component {
-
-    state = {
-        expanded: false,
-    };
-
-    handleExpandClick = () => {
-        this.setState(state => ({ expanded: !state.expanded }));
-    };
 
     render(){
 
@@ -62,30 +41,43 @@ class UserCard extends Component {
 
         return (
             <Card className={classes.card}>
-                <div className={classes.cardHead}>
-                    <CardMedia
-                        className={classes.avatar}
-                        component="img"
-                        image={IconPerson}/>
                     <CardContent className={classes.content}>
-                        <Typography variant="title" color="textSecondary">
-                            Name
-                        </Typography>
-                        <Typography variant="subheading" color="textSecondary">
-                            mail
-                        </Typography>
-                        <Typography variant="subheading" color="textPrimary">
-                            role
-                        </Typography>
-                        <CardActions className={classes.actions} disableActionSpacing>
-                            <IconButton
-                                onClick={this.handleExpandClick}
-                                aria-label="Remove">
-                                <DeleteIcon/>
-                            </IconButton>
-                        </CardActions>
+                        <Grid container direction="row" alignItems="center" spacing="16">
+                            <Grid item xs="5">
+                                <Avatar className={classes.avatar} src={this.props.user.avatar}><IconPerson className={classes.icon}/></Avatar>
+                            </Grid>
+                            <Grid item xs>
+                                <Grid container direction="column">
+                                    <Grid item>
+                                        <Typography variant="title" color="textSecondary" zeroMinWidth>
+                                            {this.props.user.username}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="subheading" color="textSecondary">
+                                            {this.props.user.rgu_id}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="subheading" color="textPrimary">
+                                            {this.props.user.role}
+                                        </Typography>
+                                    </Grid>
+                                    </Grid>
+                            </Grid>
+                        </Grid>
+                        <ListItem className={classes.mail}>
+                            <ListItemIcon><EmailIcon/></ListItemIcon>
+                            <ListItemText primary={this.props.user.email} />
+                        </ListItem>
                     </CardContent>
-                </div>
+                    <CardActions>
+                        <IconButton aria-label="Remove">
+                            <DeleteIcon/>
+                        </IconButton>
+                        <div className={classes.grow} />
+                        <Button color="primary">Edit</Button>
+                    </CardActions>
             </Card>
         )
     }
--- src/pages/UsersPage/UsersPage.js
+++ src/pages/UsersPage/UsersPage.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
+// Firebase Components
+import { firestore  } from '../../components/Firebase/Firebase';
+
 import UserCard from "./UserCard";
-import { Toolbar, Typography, Grid } from '@material-ui/core';
+import { Toolbar, Typography, Grid, InputBase, CircularProgress } from '@material-ui/core';
+import SearchIcon from "@material-ui/icons/Search";
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { purple } from '@material-ui/core/colors';
 
 
 const styles = theme => ({
@@ -12,8 +18,59 @@ const styles = theme => ({
       flexGrow: 1,
       padding: theme.spacing.unit,
     },
+    grow: {
+        flexGrow: 1,
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(purple[500], 0.15),
+        '&:hover': {
+          backgroundColor: fade(purple[500], 0.30),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing.unit,
+          width: 'auto',
+        },
+    },
+    searchIcon: {
+        color: theme.palette.common.white,
+        width: theme.spacing.unit * 9,
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+        width: '100%',
+    },
+    inputInput: {
+        color: theme.palette.common.white,
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(purple[500], 0.50),
+        paddingTop: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit * 10,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: 120,
+          '&:focus': {
+            width: 200,
+            backgroundColor: fade(purple[500], 0.75),
+          },
+        },
+    },
     grid: {
-        padding: 20,
+        width: '105%',
+        maxHeight: "80vh",
+        overflowX: "auto"
     },
     paper: {
         position: 'absolute',
@@ -22,35 +79,59 @@ const styles = theme => ({
         boxShadow: theme.shadows[5],
         padding: theme.spacing.unit * 4,
     },
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
 });
 
 class UsersPage extends Component {
 
-    state = {
-        spacing: '16',
-    };
-    
+    constructor (){
+        super();
+
+        this.unsubscribe = null;
+        this.state = {
+            users: [],
+            loading: true,
+        }
+    }
+
+    componentDidMount(){
+        this.unsubscribe = firestore.collection("users").onSnapshot(this.onCollectionUpdate)
+    }
+
+    componentWillUnmount(){
+        this.unsubscribe();
+    }
+
+    onCollectionUpdate = (querySnapshot) => {
+        var users = [];
+        querySnapshot.forEach(doc => {
+            users.push(doc.data());
+        });
+        this.setState({loading: false, users});
+    }
 
     render(){
-
         const { classes } = this.props;
-        const { spacing } = this.state;
 
         return (
             <div className={classes.root}>
                 <Toolbar>
                     <Typography component="h1" variant="display2">Manage Users</Typography>
+                    {this.state.loading && <CircularProgress className={classes.progress} />}
+                    <div className={classes.grow} />
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}><SearchIcon/></div>
+                        <InputBase placeholder="Search…" classes={{root: classes.inputRoot, input: classes.inputInput,}}/>
+                    </div>
                 </Toolbar>
-                <Grid container className={classes.grid} spacing={16}>
-                    <Grid item xs={12}>
-                        <Grid container className={classes.demo} justify="center" spacing={Number(spacing)}>
-                            {[0, 1, 2].map(value => (
-                                <Grid key={value} item>
-                                    <UserCard/>
-                                </Grid>
-                            ))}
+                <Grid className={classes.grid} container spacing={16}>
+                    {this.state.users.map(user => (
+                        <Grid item>
+                            <UserCard user={user}/>
                         </Grid>
-                    </Grid>
+                    ))}
                 </Grid>
             </div>
         );
